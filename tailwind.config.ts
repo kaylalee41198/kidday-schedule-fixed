704152cd7d42730d6901173bafdfaf202e5b1058
kaylalee41198 <kaylalee41198@gmail.com>
2026-06-07 01:49:28 -0400

    Push additional config files for build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,8 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-export default config;
